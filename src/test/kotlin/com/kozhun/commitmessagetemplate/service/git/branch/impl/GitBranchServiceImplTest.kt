8ad0541399c0a387ef957d0a84ff51ee58edcd8d
Eugene <Kozhun12345@gmail.com>
2025-05-30 06:32:14 +0300

    fix: update unit tests for GitBranchServiceImpl
    closes #41

--- src/test/kotlin/com/kozhun/commitmessagetemplate/service/git/branch/impl/GitBranchServiceImplTest.kt
+++ src/test/kotlin/com/kozhun/commitmessagetemplate/service/git/branch/impl/GitBranchServiceImplTest.kt
@@ -1,6 +1,8 @@
 package com.kozhun.commitmessagetemplate.service.git.branch.impl
 
 import com.intellij.openapi.actionSystem.AnActionEvent
+import com.intellij.openapi.application.Application
+import com.intellij.openapi.application.ApplicationManager
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.vcs.VcsDataKeys
 import com.intellij.openapi.vcs.changes.Change
@@ -14,15 +16,21 @@ import io.mockk.mockkStatic
 import io.mockk.unmockkAll
 import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
 
 class GitBranchServiceImplTest {
     private val projectMock = mockk<Project>(relaxed = true)
-    private val gitBranchMock = mockk<GitLocalBranch>()
+    private val gitBranchMock = mockk<GitLocalBranch>(relaxed = true)
     private val anActionEventMock = mockk<AnActionEvent>(relaxed = true)
 
-    private val gitBranchService = GitBranchServiceImpl(projectMock)
+    private lateinit var gitBranchService: GitBranchServiceImpl
+
+    @BeforeEach
+    fun setUp() {
+        gitBranchService = GitBranchServiceImpl(projectMock)
+    }
 
     @AfterEach
     fun tearDown() {
@@ -31,25 +39,31 @@ class GitBranchServiceImplTest {
 
     @Test
     fun `get current branch from first repository when no selected changes`() {
-        every { anActionEventMock.getData(VcsDataKeys.SELECTED_CHANGES) } returns null
         mockkStatic(GitRepositoryManager::class)
+        every { anActionEventMock.getData(VcsDataKeys.SELECTED_CHANGES) } returns null
+
         val repoMock = mockk<GitRepository> {
             every { currentBranch } returns gitBranchMock
         }
-        val repoManagerMock = mockk<GitRepositoryManager>()
-        every { repoManagerMock.repositories } returns listOf(repoMock)
+        val repoManagerMock = mockk<GitRepositoryManager> {
+            every { repositories } returns listOf(repoMock)
+        }
         every { GitRepositoryManager.getInstance(projectMock) } returns repoManagerMock
+
         val result = gitBranchService.getCurrentBranch(anActionEventMock)
         assertEquals(gitBranchMock, result)
     }
 
     @Test
     fun `should throw exception if repository list is empty when no selected changes`() {
-        every { anActionEventMock.getData(VcsDataKeys.SELECTED_CHANGES) } returns null
         mockkStatic(GitRepositoryManager::class)
-        val repoManagerMock = mockk<GitRepositoryManager>()
-        every { repoManagerMock.repositories } returns emptyList()
+        every { anActionEventMock.getData(VcsDataKeys.SELECTED_CHANGES) } returns null
+
+        val repoManagerMock = mockk<GitRepositoryManager> {
+            every { repositories } returns emptyList()
+        }
         every { GitRepositoryManager.getInstance(projectMock) } returns repoManagerMock
+
         val exception = assertThrows<IllegalStateException> {
             gitBranchService.getCurrentBranch(anActionEventMock)
         }
@@ -58,6 +72,13 @@ class GitBranchServiceImplTest {
 
     @Test
     fun `get current branch from selected change`() {
+        mockkStatic(GitRepositoryManager::class)
+        mockkStatic(ApplicationManager::class)
+
+        val appMock = mockk<Application>()
+        every { ApplicationManager.getApplication() } returns appMock
+        every { appMock.isDispatchThread } returns false
+
         val fileMock = mockk<VirtualFile>()
         val changeMock = mockk<Change> {
             every { virtualFile } returns fileMock
@@ -65,58 +86,75 @@ class GitBranchServiceImplTest {
         val repoMock = mockk<GitRepository> {
             every { currentBranch } returns gitBranchMock
         }
-        val repoManagerMock = mockk<GitRepositoryManager>()
-        every { repoManagerMock.getRepositoryForFile(fileMock) } returns repoMock
+        val repoManagerMock = mockk<GitRepositoryManager> {
+            every { getRepositoryForFile(fileMock) } returns repoMock
+        }
         every { GitRepositoryManager.getInstance(projectMock) } returns repoManagerMock
         every { anActionEventMock.getData(VcsDataKeys.SELECTED_CHANGES) } returns arrayOf(changeMock)
+
         val result = gitBranchService.getCurrentBranch(anActionEventMock)
         assertEquals(gitBranchMock, result)
     }
 
     @Test
     fun `should throw exception when repository is not found for selected file`() {
+        mockkStatic(GitRepositoryManager::class)
+        mockkStatic(ApplicationManager::class)
+
+        val appMock = mockk<Application>()
+        every { ApplicationManager.getApplication() } returns appMock
+        every { appMock.isDispatchThread } returns false
+
         val fileMock = mockk<VirtualFile>()
         val changeMock = mockk<Change> {
             every { virtualFile } returns fileMock
         }
-        val repoManagerMock = mockk<GitRepositoryManager>()
-        every { repoManagerMock.getRepositoryForFile(fileMock) } returns null
-        every { repoManagerMock.repositories } returns emptyList()
+        val repoManagerMock = mockk<GitRepositoryManager> {
+            every { getRepositoryForFile(fileMock) } returns null
+            every { repositories } returns emptyList()
+        }
         every { GitRepositoryManager.getInstance(projectMock) } returns repoManagerMock
         every { anActionEventMock.getData(VcsDataKeys.SELECTED_CHANGES) } returns arrayOf(changeMock)
+
         val exception = assertThrows<IllegalStateException> {
             gitBranchService.getCurrentBranch(anActionEventMock)
         }
-        assertEquals("Current git branch not found.", exception.message)
+        assertEquals("Git repository not found for the selected file.", exception.message)
     }
 
     @Test
     fun `get branch fallback to first repository when selected changes array is empty`() {
-        every { anActionEventMock.getData(VcsDataKeys.SELECTED_CHANGES) } returns emptyArray()
         mockkStatic(GitRepositoryManager::class)
+        every { anActionEventMock.getData(VcsDataKeys.SELECTED_CHANGES) } returns emptyArray()
+
         val repoMock = mockk<GitRepository> {
             every { currentBranch } returns gitBranchMock
         }
-        val repoManagerMock = mockk<GitRepositoryManager>()
-        every { repoManagerMock.repositories } returns listOf(repoMock)
+        val repoManagerMock = mockk<GitRepositoryManager> {
+            every { repositories } returns listOf(repoMock)
+        }
         every { GitRepositoryManager.getInstance(projectMock) } returns repoManagerMock
+
         val result = gitBranchService.getCurrentBranch(anActionEventMock)
         assertEquals(gitBranchMock, result)
     }
 
     @Test
     fun `get branch fallback to first repository when selected change has null virtual file`() {
+        mockkStatic(GitRepositoryManager::class)
         val changeMock = mockk<Change> {
             every { virtualFile } returns null
         }
         every { anActionEventMock.getData(VcsDataKeys.SELECTED_CHANGES) } returns arrayOf(changeMock)
-        mockkStatic(GitRepositoryManager::class)
+
         val repoMock = mockk<GitRepository> {
             every { currentBranch } returns gitBranchMock
         }
-        val repoManagerMock = mockk<GitRepositoryManager>()
-        every { repoManagerMock.repositories } returns listOf(repoMock)
+        val repoManagerMock = mockk<GitRepositoryManager> {
+            every { repositories } returns listOf(repoMock)
+        }
         every { GitRepositoryManager.getInstance(projectMock) } returns repoManagerMock
+
         val result = gitBranchService.getCurrentBranch(anActionEventMock)
         assertEquals(gitBranchMock, result)
     }
