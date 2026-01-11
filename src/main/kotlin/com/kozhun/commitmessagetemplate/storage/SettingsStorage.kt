b22981366dc58ce2c13b79c5136aa80b79562ace
Eugene Kozhun <Kozhun12345@gmail.com>
2026-01-11 17:36:51 +0300

    feat: use separate storage for plugin data (#79)

--- src/main/kotlin/com/kozhun/commitmessagetemplate/storage/SettingsStorage.kt
+++ src/main/kotlin/com/kozhun/commitmessagetemplate/storage/SettingsStorage.kt
@@ -3,11 +3,18 @@ package com.kozhun.commitmessagetemplate.storage
 import com.intellij.openapi.components.Service
 import com.intellij.openapi.components.SimplePersistentStateComponent
 import com.intellij.openapi.components.State
+import com.intellij.openapi.components.Storage
 import com.intellij.openapi.components.service
 import com.intellij.openapi.project.Project
 
 @Service(Service.Level.PROJECT)
-@State(name = "commit-message-template")
+@State(
+    name = "commit-message-template",
+    storages = [
+        Storage("commit-message-template.xml"),
+        Storage("misc.xml", deprecated = true)
+    ]
+)
 class SettingsStorage : SimplePersistentStateComponent<SettingsState>(getDefaultState()) {
     companion object {
         @JvmStatic
